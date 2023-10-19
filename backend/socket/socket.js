474166e2a95b949a80b3cf780c1158144b345de1
Ajay Singh <whosinghajay@gmail.com>
2023-10-19 10:47:09 +0530

    cors added in socket.js file in backend

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,12 +1,14 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
