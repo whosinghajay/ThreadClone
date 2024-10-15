8d33e52fd3a8235ba3922f66d8ecdb02d46c193f
Ajay Singh <140946442+whosinghajay@users.noreply.github.com>
2024-10-15 11:33:12 +0530

    Update serverAwake.js

--- backend/utils/serverAwake.js
+++ backend/utils/serverAwake.js
@@ -1,1 +1,10 @@
+import https from "https";
+
+const keepAwake = () => {
+  https.get("https://threadclone.onrender.com/api/test", (res) => {
+    console.log("Request sent to keep the server awake: " + res.statusCode);
+  });
+};
+
+setInterval(keepAwake, 14 * 60 * 1000);
 
